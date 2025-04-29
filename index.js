d28d968a6a24afc77f328cbcf879d542d5e91026
Daniel Sinkpon <danielsinkpon@gmail.com>
2025-04-29 18:00:14 -0400

    Add Game Summary and fix buttons bug
    Fix bug where buttons would allow more rounds
    to be played even though the game ended.
    
    Allow Game Summary to be displayed at the end
    of the game.

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const scissorsButton = document.querySelector("#scissors-btn");
 
 let humanScore = 0;
 let computerScore = 0;
-let numRounds = 0
+let numRounds = 1
+let roundsSummary = []
 
 const h3 = document.createElement('h3');
 const div = document.querySelector('#results-ctn');
@@ -15,6 +16,7 @@ const divTwo = document.createElement('div');
 const paraFour = document.createElement('p');
 const h2 = document.createElement('h2');
 const roundText = document.createElement('h2');
+const gameSummaryText = document.createElement("h2");
 
 
 div.appendChild(roundText)
@@ -41,26 +43,33 @@ function getComputerChoice(){
     return computerChoice;
 }
 
+function capitalize(s){
+    return String(s)[0].toUpperCase() + String(s).slice(1);
+}
+
 
 const playRound = function(playerChoice) {
     let computerSelection = getComputerChoice();
     paraOne.textContent = `Player Choice: ${playerChoice.toUpperCase()}`;
     paraTwo.textContent = `Computer Choice: ${computerSelection.toUpperCase()}`;
     paraThree.style.fontWeight = "600";
+    let roundWinner = ""
     switch(playerChoice){
         case 'rock':
             if (computerSelection === 'rock'){
                 paraThree.textContent = "It's a tie! Rock does not win against itself. No points alloted for anyone.";
                 paraThree.style.color = "#F5B800" // yellow
-                
+                roundWinner = "Tie";
             } else if (computerSelection === 'paper'){
                 paraThree.textContent = "You lose! Paper beats Rock. The computer gets one point.";
                 paraThree.style.color = "#A30003" // red
                 computerScore++;
+                roundWinner = "Computer";
             } else if (computerSelection === 'scissors') {
                 paraThree.textContent = "You win! Rock beats Scissors. You get one point.";
                 paraThree.style.color = "#11691A"; // green
                 humanScore++;
+                roundWinner = "Player";
             }
             break;
         case 'paper':
@@ -68,13 +77,16 @@ const playRound = function(playerChoice) {
                 paraThree.textContent = "You win! Paper beats Rock. You get one point.";
                 paraThree.style.color = "#11691A"; // green
                 humanScore++;
+                roundWinner = "Player";
             } else if (computerSelection === 'paper'){
                 paraThree.textContent = "It's a tie! Paper does not win against itself. No points alloted for anyone.";
                 paraThree.style.color = "#F5B800" // yellow
+                roundWinner = "Tie";
             } else if (computerSelection === 'scissors') {
                 paraThree.textContent = "You lose! Scissors beats Paper. The computer gets one point.";
                 paraThree.style.color = "#A30003" // red
                 computerScore++;
+                roundWinner = "Computer";
             }
             break;
         case 'scissors':
@@ -82,46 +94,44 @@ const playRound = function(playerChoice) {
                 paraThree.textContent = "You lose! Rock beats Scissors. The computer gets one point.";
                 paraThree.style.color = "#A30003" // red
                 computerScore++
+                roundWinner = "Computer";
             } else if (computerSelection === 'paper'){
                 paraThree.textContent = "You win! Scissors beats Paper. You get one point.";
                 paraThree.style.color = "#11691A"; // green
                 humanScore++;
+                roundWinner = "Player";
             } else if (computerSelection === 'scissors') {
                 paraThree.textContent = "It's a tie! Scissors does not win against itself. No points alloted for anyone.";
-                paraThree.style.color = "#F5B800" // yellow    
+                paraThree.style.color = "#F5B800" // yellow 
+                roundWinner = "Tie";
             }
             break;
     }
-    roundText.textContent = `Round ${++numRounds}`;
+    roundText.textContent = `Round ${numRounds}`;
     h3.textContent = `Score --> (PLAYER) ${humanScore} : ${computerScore} (COMPUTER)`;
+    roundsSummary.push({
+        playerChoice,
+        computerChoice: computerSelection,
+        round: numRounds++,
+        roundWinner
+    });
     if (humanScore === 5 || computerScore === 5){
+        rockButton.remove();
+        paperButton.remove();
+        scissorsButton.remove();
         let event = new CustomEvent('game-end', {
             detail: {
                 message: humanScore === 5 ? 
                 "You won. Good job!" : "You lost. No sweat. Try your luck again!",
-                color: humanScore === 5 ? "#11691A" : "#A30003"
+                color: humanScore === 5 ? "#11691A" : "#A30003",
             }
         });
         div.dispatchEvent(event);
     }
 }
 
-function resetGame(event){
-    humanScore = 0;
-    computerScore = 0;
-    paraOne.textContent = "";
-    paraTwo.textContent = "";
-    paraThree.textContent = "";
-    h3.textContent = "";
-    divTwo.textContent = "";
-    paraFour.textContent = "" ;
-    h2.textContent = "" ;
-    rockButton.disabled = false;
-    paperButton.disabled = false;
-    scissorsButton.disabled = false;
-    roundText.textContent = "";
-    event.target.remove();
-    numRounds = 0;
+function resetGame(){
+    location.reload()
 }
 
 function selectPlayerChoice(event){
@@ -134,21 +144,66 @@ function selectPlayerChoice(event){
     }
 }
 
-rockButton.addEventListener('click', selectPlayerChoice);
-paperButton.addEventListener('click', selectPlayerChoice);
-scissorsButton.addEventListener('click', selectPlayerChoice);
-div.addEventListener('game-end', function(e) {
-    paraFour.textContent = e.detail.message;
-    paraFour.style.color = e.detail.color;
+
+function createTable(statsTable){
+    const thead = document.createElement('thead');
+    const tbody = document.createElement('tbody');
+    const tr = document.createElement('tr');
+    const roundHeader = document.createElement('th');
+    const playerChoiceHeader = document.createElement('th');
+    const computerChoiceHeader = document.createElement('th');
+    roundHeader.textContent = "Round";
+    playerChoiceHeader.textContent = "Player Choice";
+    computerChoiceHeader.textContent = "Computer Choice";
+    tr.appendChild(roundHeader);
+    tr.appendChild(playerChoiceHeader);
+    tr.appendChild(computerChoiceHeader);
+    thead.appendChild(tr)
+    statsTable.appendChild(thead);
+    roundsSummary.forEach(function(roundInfo){
+        const roundWinner = roundInfo.roundWinner;
+        const tr = document.createElement('tr');
+        const roundNumberInfo = document.createElement('th');
+        const playerChoiceInfo = document.createElement('th');
+        const computerChoiceInfo = document.createElement('th');
+        roundNumberInfo.textContent = roundInfo.round;
+        playerChoiceInfo.textContent = capitalize(roundInfo.playerChoice);
+        playerChoiceInfo.style.backgroundColor = roundWinner === "Player" ? "#11691A" : roundWinner === "Computer" ? "#A30003" : "#F5B800";
+        playerChoiceInfo.style.color = roundWinner === "Player" ? "#fff" : roundWinner === "Computer" ? "#fff" : "#000";
+        computerChoiceInfo.textContent = capitalize(roundInfo.computerChoice);
+        computerChoiceInfo.style.backgroundColor = roundWinner === "Computer" ? "#11691A" : roundWinner === "Player" ? "#A30003" : "#F5B800";
+        computerChoiceInfo.style.color = roundWinner === "Player" ? "#fff" : roundWinner === "Computer" ? "#fff" : "#000";
+        tr.appendChild(roundNumberInfo);
+        tr.appendChild(playerChoiceInfo);
+        tr.appendChild(computerChoiceInfo);
+        tbody.append(tr);
+    })
+    statsTable.appendChild(tbody);
+}
+
+function showResults(event) {
+    paraFour.textContent = event.detail.message;
+    paraFour.style.color = event.detail.color;
     paraFour.style.fontWeight = "600";
     rockButton.disabled = true;
     paperButton.disabled = true;
     scissorsButton.disabled = true;
+    const statsTable = document.createElement('table');
+    createTable(statsTable);
     h2.textContent = `FINAL SCORE --> (PLAYER) ${humanScore} : ${computerScore} (COMPUTER)`;
+    gameSummaryText.textContent = "Game Summary"
+    gameSummaryText.style.marginTop = "20px";
     const button = document.createElement('button');
     button.textContent = 'PLAY AGAIN';
     button.addEventListener('click', resetGame);
     div.appendChild(paraFour);
     div.appendChild(h2);
     div.appendChild(button);
-})
+    div.appendChild(gameSummaryText);
+    div.appendChild(statsTable);
+}
+
+rockButton.addEventListener('click', selectPlayerChoice);
+paperButton.addEventListener('click', selectPlayerChoice);
+scissorsButton.addEventListener('click', selectPlayerChoice);
+div.addEventListener('game-end', showResults)
